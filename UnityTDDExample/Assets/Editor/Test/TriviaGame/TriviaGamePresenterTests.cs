f35e90d424beea620498e57e230092cfa913ee02
Valeria Colombo <valeria.colombo@etermax.com>
2019-05-06 11:03:24 -0300

    Initial commit

---
+++ UnityTDDExample/Assets/Editor/Test/TriviaGame/TriviaGamePresenterTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace Test.TriviaGame
+{
+    [TestFixture]
+    public class TriviaGamePresenterTests
+    {
+        [Test]
+        public void DummyTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
